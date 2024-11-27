43e61a7286f0d11113037f58f3978dc5d784db4d
Itai Luz <etay.luz@walmart.com>
2024-11-26 18:11:13 -0600

    Initial Commit

---
+++ Companies/CompaniesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompaniesApp.swift
+//  Companies
+//
+//  Created by ETAY LUZ - Vendor on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CompaniesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
